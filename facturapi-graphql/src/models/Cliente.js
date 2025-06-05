f665cc1035923e72010a928480a8a10d55d54cb1
ReacherDaniel <105395995+ReacherDaniel@users.noreply.github.com>
2025-06-05 03:52:01 -0700

    Add files via upload

---
+++ facturapi-graphql/src/models/Cliente.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ClienteSchema = new mongoose.Schema({
+  facturapi_id: String,      // ID que regresa FacturAPI
+  legal_name: String,
+  tax_id: String,
+  tax_system: String,
+  email: String,
+  phone: String,
+  address: {
+    zip: String,
+    street: String,
+    exterior: String,
+    neighborhood: String,
+    city: String,
+    municipality: String,
+    state: String,
+    country: String
+  }
+});
+
+module.exports = mongoose.model("Cliente", ClienteSchema);
